d01a15bb73a52aae3d7a898b66ff021c367d69c1
BauweraertsWouter <wouter.bauweraerts@telenet.be>
2017-04-15 09:38:16 +0200

    Added first test for TicketManager. Added packages: Xunit, NSubstitute and NBuilder (generic builder)

---
+++ Tests/TicketManagerTests.cs
@@ -0,0 +1,33 @@
+﻿using SC.BL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FizzWare.NBuilder;
+using NSubstitute;
+using NSubstitute.Core;
+using SC.BL.Domain;
+using SC.DAL;
+using Xunit;
+
+namespace Tests
+{
+    
+    public class TicketManagerTests
+    {
+        private ITicketRepository _repository;
+        private ITicketManager _mgr;
+
+        [Fact]
+        public void GetTicketsCallsRepositoryReadTicketsExactlyOnce()
+        {
+            _repository = Substitute.For<ITicketRepository>();
+            _mgr = new TicketManager(_repository);
+            _mgr.GetTickets();
+            _repository.Received(1).ReadTickets();
+        }
+
+
+    }
+}
